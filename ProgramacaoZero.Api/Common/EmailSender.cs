1f8962b608ec3f6e1b965e58ade52672f3f9d0f4
Renato Gava <renatogava2@live.com>
2023-12-07 15:20:21 -0300

    removendo método inutilizado

--- ProgramacaoZero.Api/Common/EmailSender.cs
+++ ProgramacaoZero.Api/Common/EmailSender.cs
@@ -36,34 +36,5 @@ namespace ProgramacaoZero.Api.Common
                 smtp.Send(mail);
             }
         }
-
-        public void EnviarEmail(string toEmail, string assunto, string corpo)
-        {
-            var fromEmail = "contato@renatogava.com.br";
-            var fromPassword = "5yoJ8x21JAw%";
-            var fromHost = "smtp.zoho.com";
-            var fromPort = 587;
-
-            MailMessage mail = new MailMessage()
-            {
-                From = new MailAddress(fromEmail)
-            };
-
-            mail.To.Add(new MailAddress(toEmail));
-
-            mail.Subject = assunto;
-            mail.Body = corpo;
-            mail.IsBodyHtml = true;
-            mail.Priority = MailPriority.High;
-
-            using (SmtpClient smtp = new SmtpClient(fromHost, fromPort))
-            {
-                smtp.UseDefaultCredentials = false;
-                smtp.Credentials = new NetworkCredential(fromEmail, fromPassword);
-                smtp.EnableSsl = true;
-                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-                smtp.Send(mail);
-            }
-        }
     }
 }
